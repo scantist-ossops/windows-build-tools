15176c9fc1fd9f55304f0551145d4f5ada655007
Jackie <jwtan2010@gmail.com>
2023-12-01 15:46:33 +0800

    Applied patches to tag v0.7.0

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ var pythonMirror = process.env.PYTHON_MIRROR || 'https://www.python.org/ftp/pyth
 
 var buildTools = {
   installerName: 'BuildTools_Full.exe',
-  installerUrl: 'http://download.microsoft.com/download/5/f/7/5f7acaeb-8363-451f-9425-68a90f98b238/visualcppbuildtools_full.exe',
+  installerUrl: 'https://download.microsoft.com/download/5/f/7/5f7acaeb-8363-451f-9425-68a90f98b238/visualcppbuildtools_full.exe',
   logName: 'build-tools-log.txt'
 }
 
